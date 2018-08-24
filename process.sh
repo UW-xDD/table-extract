066f805c2267a379c907d76ff9fdfe73b3da5647
Maximilian Konzack <maximilian.konzack@idiv.de>
2018-08-24 09:49:49 +0200

    Renaming of annotate.py

--- process.sh
+++ process.sh
@@ -3,4 +3,4 @@
 tesseract $1/png/page_$2.png $1/tesseract-txt/page_$2 txt
 tesseract $1/png/page_$2.png $1/tesseract/page_$2.html hocr
 mv $1/tesseract/page_$2.html.hocr $1/tesseract/page_$2.html
-python annotate.py $1/tesseract/page_$2.html $1/annotated/page_$2.png
+python ocr_annotations.py $1/tesseract/page_$2.html $1/annotated/page_$2.png
